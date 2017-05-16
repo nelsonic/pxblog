b3ba9cc53ffd2233da216953b92095a2574f4a75
nelsonic <contact.nelsonic+github@gmail.com>
2017-05-16 08:33:11 +0100

    update config/prod.secret.exs to use environment variables

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :pxblog, Pxblog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "pxblog_dev",
-  hostname: "localhost",
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: System.get_env("DATABASE_NAME"),
+  hostname: System.get_env("DATABASE_HOST"),
   pool_size: 10
